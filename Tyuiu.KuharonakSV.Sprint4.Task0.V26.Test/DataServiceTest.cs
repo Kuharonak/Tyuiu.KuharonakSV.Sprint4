002074163451f18224f135cffdfb8316f9230f28
СЕГА <skuhronak@gmail.com>
2025-11-24 22:11:45 +0300

    Task0

---
+++ Tyuiu.KuharonakSV.Sprint4.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KuharonakSV.Sprint4.Task0.V26.Lib;
+namespace Tyuiu.KuharonakSV.Sprint4.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            int res = ds.GetSumOddArrEl(array);
+            int wait = 41;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint4.Task0.V26/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KuharonakSV.Sprint4.Task0.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму нечетных *");
+        Console.WriteLine("* элементов                                                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] x = new int[10] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+        Console.WriteLine("{9 ,3 ,7 ,1 ,5 ,5 ,3 ,2 ,1 ,7}");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        int res = ds.GetSumOddArrEl(x);
+        Console.WriteLine("Сумма нечетных элементов массива = " + res);
+        Console.ReadKey();
+    }
+}
