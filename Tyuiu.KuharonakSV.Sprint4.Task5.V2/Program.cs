2d5df239b806688a29126375c2633258d12b8e77
СЕГА <skuhronak@gmail.com>
2025-11-28 16:04:32 +0300

    Task5

---
+++ Tyuiu.KuharonakSV.Sprint4.Task5.V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KuharonakSV.Sprint4.Task5.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+        
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* случайными значениями в диапазоне от -7 до 5. Найти количество          *");
+        Console.WriteLine("* отрицательных элементов.                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] matrix = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i, j] = rnd.Next(-4, 6);
+            }
+        }
+
+        Console.WriteLine("\nИсходный массив:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        var result = ds.Calculate(matrix);
+        Console.WriteLine("Количество положительных элементов = " + result);
+        Console.ReadKey();
+    }
+}
